7d29121684897e300fcd01eec478fd5ba366b4f6
maria-silvas <maria@rigbie.com.br>
2022-11-29 17:08:41 -0300

    alteração

--- src/model/Pista.java
+++ src/model/Pista.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class Pista {
     private String numero;
 
+    private int id;
+
     public static ArrayList<Pista> pistas = new ArrayList<Pista>();
 
     public Pista(int id, String numero) {
@@ -17,6 +19,27 @@ public class Pista {
 
     public void setNumero(String numero) {
         this.numero = numero;
+    }
+
+    public static Pista getPistaById(int id) {
+        for (Pista pista : Pista.pistas) {
+            if (pista.id == id) {
+                return pista;
+            }
+        }
+
+        return null;
+    }
+
+    public static Pista deletePistaById(int id) {
+        for (Pista pista : Pista.pistas) {
+            if (pista.id == id) {
+                Pista.pistas.remove(pista);
+                return pista;
+            }
+        }
+
+        return null;
     }
 
     @Override
