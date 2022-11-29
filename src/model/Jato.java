faa647dea7065a67ec1e9bc7f2456465ed1c3596
maria-silvas <maria@rigbie.com.br>
2022-11-29 17:03:50 -0300

    alteração

--- src/model/Jato.java
+++ src/model/Jato.java
@@ -1,30 +1,24 @@
 import java.util.ArrayList;
 
 public class Jato<Int> extends Aeoronave {
-    private String marca;
+    
     private String cor;
-    private String modelo;
+   
     private int velocidade;
 
     public static ArrayList<Jato> jatos = new ArrayList<Jato>();
 
     public Jato(int id, String nome, String marca, String cor, String modelo, int velocidade) {
         super(id, nome);
-        this.marca = marca;
+        
         this.cor = cor;
         this.modelo = modelo;
-        this.velocidade = velocidade;
+       
 
         jatos.add(this);
     }
 
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+    
 
     public String getCor() {
         return cor;
@@ -34,13 +28,6 @@ public class Jato<Int> extends Aeoronave {
         this.cor = cor;
     }
 
-    public String getModelo(String modelo) {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
 
     public int getVelocidade(int velocidade) {
         return velocidade;
@@ -52,7 +39,7 @@ public class Jato<Int> extends Aeoronave {
 
     @Override
     public String toString() {
-        return super.toString() + "Marca" + this.marca + "| Cor" + this.cor + "| Modelo:" + this.modelo + "| Velocidade"
+        return super.toString() + "| Cor" + this.cor + "| Velocidade"
                 + this.velocidade;
     }
 
